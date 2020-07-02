b832a1c74eb38a31378545dd823e8cfc76861044
lyj <LYJ112233>
2020-07-02 18:26:16 +0800

    Sentinel

--- cloud-alibaba-stater-sentinel-8848/src/main/java/org/augustus/sentinel/controller/SentinelController.java
+++ cloud-alibaba-stater-sentinel-8848/src/main/java/org/augustus/sentinel/controller/SentinelController.java
@@ -1,7 +1,10 @@
 package org.augustus.sentinel.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
@@ -20,7 +23,19 @@ public class SentinelController {
     }
 
     @GetMapping("/b")
-    public String testB() {
+    public String testB() throws InterruptedException {
         return "sentinel B";
+    }
+
+    @GetMapping("/hotKey")
+    @SentinelResource(value = "hotKey", blockHandler = "hotKeyHandler")
+    public String hotKey(@RequestParam(required = false) String p1, @RequestParam(required = false) String p2) {
+        return "----hotKey----";
+    }
+
+    public String hotKeyHandler(String p1, String p2, BlockException exception) {
+        System.out.println("p1: " + p1 + ", " + "p2: " + p2);
+        System.out.println(exception.getMessage());
+        return "custom fallback";
     }
 }
